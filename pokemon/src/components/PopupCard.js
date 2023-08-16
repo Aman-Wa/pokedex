59a95823ffc5fa70e91c4f2dd45397c8e5f6f972
Aman W <aman.walia@ria.insure>
2023-08-16 10:17:21 +0530

    popupcard2

--- pokemon/src/components/PopupCard.js
+++ pokemon/src/components/PopupCard.js
@@ -6,7 +6,7 @@ const PopupCard = ({ pokemon, onClose }) => {
     <div className="popup-overlay">
       <div className="popup-content">
         <button className="close-button" onClick={onClose}>
-          &#10005; {/* Unicode character for close (X) */}
+          &#10005; 
         </button>
         <h2>{pokemon.name}</h2>
         <img src={pokemon.img} alt={pokemon.name} />
